15cf3644e213572e9dda1339703abc48cbc33112
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-03-09 14:12:37 +0100

    draw player

--- main.py
+++ main.py
@@ -3,42 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
-
-
-
-#unused clock object
-class Clock:
-    def __init__(self, fps: int = 60):
-        self._fps = fps
-        self._clock: pygame.time.Clock = pygame.time.Clock()
-
-    @property
-    def fps(self):
-        return self._fps
-    
-    def set_framerate(self, fps: int):
-        self._fps = fps
-
-    def tick(self):
-        self._clock.tick(self._fps)
-        
+from player import *    
 
 def main():
     dt = 0
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_clock = pygame.time.Clock() 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_clock = pygame.time.Clock()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(F"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
