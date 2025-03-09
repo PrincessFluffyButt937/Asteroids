737795498e4eb0acc7dbd04d878559380bbe814f
PrincessFluffyButt937 <jhosch@seznam.cz>
2025-03-09 11:31:41 +0100

    fps stage done

--- main.py
+++ main.py
@@ -4,14 +4,37 @@
 import pygame
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+#unused clock object
+class Clock:
+    def __init__(self, fps: int = 60):
+        self._fps = fps
+        self._clock: pygame.time.Clock = pygame.time.Clock()
+
+    @property
+    def fps(self):
+        return self._fps
+    
+    def set_framerate(self, fps: int):
+        self._fps = fps
+
+    def tick(self):
+        self._clock.tick(self._fps)
+        
 
 def main():
+    dt = 0
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock() 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         pygame.Surface.fill(screen, (0,0,0))
-
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
